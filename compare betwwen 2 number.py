71ff26aa962fc1311c2f9564c2e5e40ef9fb21e4
20Dartside <vikaspatel9922@gmail.com>
2025-02-18 13:10:56 +0530

    Add files via upload

---
+++ compare betwwen 2 number.py
@@ -0,0 +1,9 @@
+a=float(input("enter first number"))
+b=float(input("enter  second number "))
+
+if a>b:
+    print ('number is greater ')
+elif a==b:
+    print('number is equal')
+else:
+    print('number is smaller')
---
+++ even_odd.py
@@ -0,0 +1,8 @@
+a=int(input("enter any number:"))
+result="number is even" if a%2==0 else "number is odd"
+print(result)
+
+##if a%2==0:
+##    print("number is even")
+##else:
+##    print("number is odd")
